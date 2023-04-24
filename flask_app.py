cb6389640500d676a429a8b1d5c0b705a246d3b7
charlesleiserson <jgoodwell2376@gmail.com>
2023-04-24 19:02:45 +0000

    s7s7s7s77s77s

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,19 @@
 from flask import Flask, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="charlesleiserson",
+    password="Exeter99",
+    hostname="charlesleiserson.mysql.pythonanywhere-services.com",
+    databasename="charlesleiserson$comments",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
 
 comments = []
 
